9143758c77d657f8919e0cd9c8eeb9f3802490f5
Jaumenset <jaumen81@gmail.com>
2020-04-14 12:45:34 +0200

    Terminada screen de info serie

--- screens/serieScreen.js
+++ screens/serieScreen.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image, ActivityIndicator, ScrollView, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image, ActivityIndicator, ScrollView, SafeAreaView, Button } from 'react-native';
 import {ToastAndroid} from 'react-native';
 import TVDB from 'node-tvdb';
 import config from './config';
 import constants from './constants';
-import { Divider } from 'react-native-elements';
+import { Divider, Icon } from 'react-native-elements';
 
 
 async function getSeriesById(id) {
@@ -32,10 +32,23 @@ async function getSeriesById(id) {
 
 function SeccionesLaterales(props) {
   return (
-    <View style={{flex: 1, flexDirection: "row"}}>
+    <View style={{flexDirection: "row"}}>
       <Text style={styles.seccionesTextTitle}>{props.name}: </Text>
       <Text style={styles.seccionesTextValue}>{props.value}</Text>
+      
     </View>
+  )
+}
+
+function TemporadasComponent(props) {
+  return (
+    <TouchableOpacity style={styles.temporadaStyle}>
+      <Text style={styles.temporadaTitle}>Temporada {props.numeroTemporada}</Text>
+      <View style={{flexDirection: "row"}}>
+        <Text style={styles.episodioStyle}>{props.numeroEpisodios}</Text>
+        <Icon name='right' type='antdesign'></Icon>
+      </View>
+    </TouchableOpacity>
   )
 }
 
@@ -55,11 +68,18 @@ export default class RegisterScreen extends Component {
       estadoSerie: "",
       platformSerie: "",
       numTemporadas: "",
+      temporadas: null,
+      descripcionCorta: "",
+      existeDescripcionCorta: false
     }
   }
 
   getSeriesById = async function(id) {
     
+  }
+
+  onPressShowMore = () => {
+    this.setState({existeDescripcionCorta: false})
   }
   componentDidMount() {
     this.setState({status: constants.WAITING});
@@ -67,6 +87,7 @@ export default class RegisterScreen extends Component {
     tvdb.language = "es";
     console.log(config.tvdb_key);
     console.log(this.state.idSerie);
+
     
     /*getSeriesById(this.state.idSerie)
       .then(response => {
@@ -79,6 +100,22 @@ export default class RegisterScreen extends Component {
     tvdb.getSeriesAllById(this.state.idSerie)
       .then(response => {
         console.log(response);
+        let auxTemporada = [];
+        for (let i = 1; i<=response.season; i++) {
+          console.log(i);
+          auxTemporada.push({
+            num: i,
+            episodios: 0,
+            idTemporada: 0
+          });
+        }
+        for(let e of response.episodes) {
+          if(e.airedSeason != 0 && e.airedSeason<=auxTemporada.length) {
+            let t = auxTemporada[e.airedSeason-1];
+            t.episodios++;
+            t.idTemporada = e.airedSeasonID;
+          } 
+        }
         this.setState({title: response.seriesName,
             imagenSerie: URLPoster + response.poster,
             descripcion: response.overview,
@@ -86,7 +123,18 @@ export default class RegisterScreen extends Component {
             estadoSerie: response.status,
             numTemporadas: response.season,
             platformSerie: response.network,
+            temporadas: auxTemporada,
             status: constants.OK});
+
+        let auxDescripcion = this.state.descripcion.split(" ");
+        if (auxDescripcion.length > 50) {
+          this.setState({
+            descripcionCorta: auxDescripcion.slice(0, 49).join(" ")+"...",
+            existeDescripcionCorta: true,
+          })
+        }
+        console.log(auxDescripcion.length);
+        
       }).catch(error => {
         console.info(error);
         this.setState({status: constants.ERROR})
@@ -125,7 +173,7 @@ export default class RegisterScreen extends Component {
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <ScrollView>
+        <ScrollView style={{width:"100%"}}>
         { this.state.status==constants.WAITING && (
           <View>
             <ActivityIndicator
@@ -142,33 +190,45 @@ export default class RegisterScreen extends Component {
               {this.state.title}
             </Text>
           </View>
-          <View style={{flex: 1, flexDirection: "row", alignContent: "center", justifyContent: 'center', alignItems: "center"}}>
+          <View style={{flexDirection: "row", alignContent: "center", justifyContent: 'center', alignItems: "center"}}>
             <View style={{flex: 1, height: 275}}>
               <Image
                 style={styles.imageStyle}
                 source={{uri: this.state.imagenSerie}}>
               </Image>
             </View>
-              <View style={{flex: 1, flexDirection: "column"}}>
-                <SeccionesLaterales name={"Rating"} value={this.state.puntuacionSerie}></SeccionesLaterales>
-                <SeccionesLaterales name={"Temporadas"} value={this.state.numTemporadas}></SeccionesLaterales>
-                <SeccionesLaterales name={"Plataforma"} value={this.state.platformSerie}></SeccionesLaterales>
-                <SeccionesLaterales name={"Estado"} value={this.state.estadoSerie}></SeccionesLaterales>
+              <View style={{flex: 1, flexDirection: "column", justifyContent: 'flex-start', height: "100%"}}>
+                <SeccionesLaterales name="Rating" value={this.state.puntuacionSerie}></SeccionesLaterales>
+                <SeccionesLaterales name="Temporadas" value={this.state.numTemporadas}></SeccionesLaterales>
+                <SeccionesLaterales name="Plataforma" value={this.state.platformSerie}></SeccionesLaterales>
+                <SeccionesLaterales name="Estado" value={this.state.estadoSerie}></SeccionesLaterales>
               </View>
           </View>
           <Divider
           style={{backgroundColor: "#ffc045", borderWidth: 3, height: 8, borderColor: "#065471"}}>
           </Divider>
-          <View style={{ flex: 1, alignContent: 'center', justifyContent: 'center', alignItems: 'center', flexDirection: "column"}}>
-            <View style={{flex: 2}}>
-            <Text style={styles.sinopsisText}>
-                Sinopsis
-              </Text>
+          { !this.state.existeDescripcionCorta && (
+            <View style={{alignContent: 'center', justifyContent: 'center', alignItems: 'center', flexDirection: "column", marginBottom: 8}}>
               <Text style={styles.descripcionText}>
                 {this.state.descripcion}
               </Text>
             </View>
-          </View>
+          )}
+          { this.state.existeDescripcionCorta && (
+            <View style={{alignContent: 'center', justifyContent: 'center', alignItems: 'center', flexDirection: "column", marginBottom: 8}}>
+              <Text style={styles.descripcionText}>
+                {this.state.descripcionCorta}
+              </Text>
+              <TouchableOpacity style={styles.leerMasButton} onPress={this.onPressShowMore}>
+                <Text style={{color: "white", fontFamily: "sans-serif"}}>Mostrar más...</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+          {this.state.temporadas.map((item) => {
+            return (
+              <TemporadasComponent numeroTemporada={item.num} numeroEpisodios={item.episodios}></TemporadasComponent>
+              );
+          })}
           </React.Fragment>
         )}
         </ScrollView>
@@ -185,6 +245,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#282828',
     alignContent: "center"
   },
+  leerMasButton: {
+    width: 120,
+    borderRadius: 5,
+    backgroundColor: "#065471",
+    padding: 5,
+    alignItems: "center"
+  },
   textoLogin: {
     fontSize: 20,
     color: "white"
@@ -195,7 +262,7 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     marginRight: 5,
     padding: 7,
-    fontFamily: "serif"
+    fontFamily: "sans-serif"
   },
   sinopsisText: {
     fontSize: 20,
@@ -203,23 +270,24 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     marginRight: 5,
     padding: 7,
-    fontFamily: "serif"
+    fontFamily: "sans-serif",
+    alignSelf: "flex-start"
   },
   seccionesTextTitle: {
-    fontSize: 20,
+    fontSize: 18,
     color: "#fafafa",
     marginLeft: 5,
-    marginRight: 5,
-    padding: 7,
-    fontFamily: "serif"
+    marginRight: 0,
+    paddingBottom: 5,
+    fontFamily: "sans-serif"
   },
   seccionesTextValue: {
-    fontSize: 20,
+    fontSize: 18,
     color: "#ffc045",
-    marginLeft: 5,
+    marginLeft: 0,
     marginRight: 5,
-    padding: 7,
-    fontFamily: "serif"
+    paddingBottom: 5,
+    fontFamily: "sans-serif"
   },
   titleText: {
     margin: 5,
@@ -228,7 +296,7 @@ const styles = StyleSheet.create({
     color: "#ffc045",
     padding: 5,
     alignSelf: "center",
-    fontFamily: "serif"
+    fontFamily: "sans-serif"
   },
   button: {
     width: 275,
@@ -253,5 +321,30 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     alignSelf: "center",
     marginTop: 5
+  },
+  temporadaStyle: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "90%",
+    backgroundColor: "#ffc045",
+    borderRadius: 5,
+    alignSelf: "center",
+    padding: 8,
+    marginVertical: 8,
+  },
+  temporadaTitle: {
+    fontSize: 20,
+    fontFamily: "sans-serif",
+    color: "black"
+  },
+  episodioStyle: {
+    alignSelf: "center",
+    marginRight: 8,
+    backgroundColor: "#ffd98f",
+    borderRadius: 10,
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    color: "#33260e"
   }
+
 });
